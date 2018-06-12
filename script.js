fe4c284e398a07d8e7bddf17095e514b8d54f242
RogerFontBruch <39918412+RogerFontBruch@users.noreply.github.com>
2018-06-12 22:13:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,223 @@
+
+function passaPaginaMar(){
+    var paginaMar=document.getElementById('pagina2');
+    paginaMar.style.visibility='hidden';
+    var paginaZonaMar=document.getElementById('pagina3');
+    paginaZonaMar.style.visibility='visible';
+}
+
+function passaPaginaRiu(){
+    var paginaRiu=document.getElementById('pagina2');
+    paginaRiu.style.visibility='hidden';
+    var paginaZonaRiu=document.getElementById('pagina4');
+    paginaZonaRiu.style.visibility='visible';
+    
+    
+}
+
+function passaPaginaMarPunt(){
+    var paginaZonaMar=document.getElementById('pagina3');
+    paginaZonaMar.style.visibility='hidden';
+    var marZona1=document.getElementById('pagina5');
+    marZona1.style.visibility='visible';
+}
+function passaPaginaMarPont(){
+    var paginaZonaMar=document.getElementById('pagina3');
+    paginaZonaMar.style.visibility='hidden';
+    var marZona2=document.getElementById('pagina6');
+    marZona2.style.visibility='visible';
+}
+
+function passaPaginaRiuPunt(){
+    var paginaZonaRiu=document.getElementById('pagina4');
+    paginaZonaRiu.style.visibility='hidden';
+    var riuZona1=document.getElementById('pagina7');
+    riuZona1.style.visibility='visible';
+}
+function passaPaginaRiuPont(){
+    var paginaZonaRiu=document.getElementById('pagina4');
+    paginaZonaRiu.style.visibility='hidden';
+    var riuZona2=document.getElementById('pagina8');
+    riuZona2.style.visibility='visible';
+}
+
+
+
+
+function passaPagina2minMar(){
+    var marZona1=document.getElementById('pagina5');
+    marZona1.style.visibility='hidden';
+    var mar2Min=document.getElementById('minmar');
+    mar2Min.style.visibility='visible';
+    
+}
+
+function passaPagina2minMar2(){
+    var marZona2=document.getElementById('pagina6');
+    marZona2.style.visibility='hidden';
+    var mar2Min2=document.getElementById('minmar2');
+    mar2Min2.style.visibility='visible';
+    
+    
+}
+
+function passaPagina2minRiu(){
+    var riuZona1=document.getElementById('pagina7');
+    riuZona1.style.visibility='hidden';
+    var riu2Min=document.getElementById('minriu');
+    riu2Min.style.visibility='visible';
+    
+}
+
+function passaPagina2minRiu2(){
+    var riuZona2=document.getElementById('pagina8');
+    riuZona2.style.visibility='hidden';
+    var riu2Min2=document.getElementById('minriu2');
+    riu2Min2.style.visibility='visible';
+    
+}
+function passaPagina30minMar(){
+    var marZona1=document.getElementById('pagina5');
+    marZona1.style.visibility='hidden';
+    var mar30Min=document.getElementById('minsmar');
+    mar30Min.style.visibility='visible';
+    
+    
+}
+
+function passaPagina30minMar2(){
+    var marZona2=document.getElementById('pagina6');
+    marZona2.style.visibility='hidden';
+    var mar30Min2=document.getElementById('minsmar2');
+    mar30Min2.style.visibility='visible';
+    
+    
+}
+
+function passaPagina30minRiu(){
+    var riuZona1=document.getElementById('pagina7');
+    riuZona1.style.visibility='hidden';
+    var riu30Min=document.getElementById('minsriu');
+    riu30Min.style.visibility='visible';
+    
+    
+}
+
+function passaPagina30minRiu2(){
+    var riuZona2=document.getElementById('pagina8');
+    riuZona2.style.visibility='hidden';
+    var riu30Min2=document.getElementById('minsriu2');
+    riu30Min2.style.visibility='visible';
+    
+    
+}
+function passaPagina2hMar(){
+    var marZona1=document.getElementById('pagina5');
+    marZona1.style.visibility='hidden';
+    var mar2H=document.getElementById('hmar');
+    mar2H.style.visibility='visible';
+    
+    
+}
+
+function passaPagina2hMar2(){
+    var marZona2=document.getElementById('pagina6');
+    marZona2.style.visibility='hidden';
+    var mar2H2=document.getElementById('hmar2');
+    mar2H2.style.visibility='visible';
+    
+    
+}
+
+function passaPagina2hRiu(){
+    var riuZona1=document.getElementById('pagina7');
+    riuZona1.style.visibility='hidden';
+    var riu2H=document.getElementById('hriu');
+    riu2H.style.visibility='visible';
+    
+    
+}
+
+function passaPagina2hRiu2(){
+    var riuZona2=document.getElementById('pagina8');
+    riuZona2.style.visibility='hidden';
+    var riu2H2=document.getElementById('hriu2');
+    riu2H2.style.visibility='visible';
+    
+    
+}
+function passaPaginaInici1(){
+    var tornarpesca1=document.getElementById('minmar');
+    tornarpesca1.style.visibility='hidden';
+    var paginaZonaMar=document.getElementById('pagina3');
+    paginaZonaMar.style.visibility='visible';
+}
+function passaPaginaInici2(){
+    var tornarpesca2=document.getElementById('minmar2');
+    tornarpesca2.style.visibility='hidden';
+    var paginaZonaMar=document.getElementById('pagina3');
+    paginaZonaMar.style.visibility='visible';
+}
+function passaPaginaInici3(){
+    var tornarpesca3=document.getElementById('minriu');
+    tornarpesca3.style.visibility='hidden';
+    var paginaZonaRiu=document.getElementById('pagina4');
+    paginaZonaRiu.style.visibility='visible';
+}
+function passaPaginaInici4(){
+    var tornarpesca4=document.getElementById('minriu2');
+    tornarpesca4.style.visibility='hidden';
+    var paginaZonaRiu=document.getElementById('pagina4');
+    paginaZonaRiu.style.visibility='visible';
+}
+function passaPaginaInici5(){
+    var tornarpesca5=document.getElementById('minsmar');
+    tornarpesca5.style.visibility='hidden';
+    var paginaZonaMar=document.getElementById('pagina3');
+    paginaZonaMar.style.visibility='visible';
+}
+function passaPaginaInici6(){
+    var tornarpesca6=document.getElementById('minsmar2');
+    tornarpesca6.style.visibility='hidden';
+    var paginaZonaMar=document.getElementById('pagina3');
+    paginaZonaMar.style.visibility='visible';
+}
+function passaPaginaInici7(){
+    var tornarpesca7=document.getElementById('minsriu');
+    tornarpesca7.style.visibility='hidden';
+    var paginaZonaRiu=document.getElementById('pagina4');
+    paginaZonaRiu.style.visibility='visible';
+}
+function passaPaginaInici8(){
+    var tornarpesca8=document.getElementById('minsriu2');
+    tornarpesca8.style.visibility='hidden';
+    var paginaZonaRiu=document.getElementById('pagina4');
+    paginaZonaRiu.style.visibility='visible';
+}
+function passaPaginaInici9(){
+    var tornarpesca9=document.getElementById('hmar');
+    tornarpesca9.style.visibility='hidden';
+    var paginaZonaMar=document.getElementById('pagina3');
+    paginaZonaMar.style.visibility='visible';
+}
+function passaPaginaInici10(){
+    var tornarpesca10=document.getElementById('hmar2');
+    tornarpesca10.style.visibility='hidden';
+    var paginaZonaMar=document.getElementById('pagina3');
+    paginaZonaMar.style.visibility='visible';
+}
+function passaPaginaInici11(){
+    var tornarpesca11=document.getElementById('hriu');
+    tornarpesca11.style.visibility='hidden';
+    var paginaZonaRiu=document.getElementById('pagina4');
+    paginaZonaRiu.style.visibility='visible';
+}
+function passaPaginaInici12(){
+    var tornarpesca12=document.getElementById('hriu2');
+    tornarpesca12.style.visibility='hidden';
+    var paginaZonaRiu=document.getElementById('pagina4');
+    paginaZonaRiu.style.visibility='visible';
+}
+
+
+
